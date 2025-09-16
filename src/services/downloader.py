c2839080ffb3bc94ae44eade72db4e9afb672589
xiaolong70701 <xiaolong70701@gmail.com>
2025-09-16 23:23:47 +0800

    feat: return zip directly

--- src/services/downloader.py
+++ src/services/downloader.py
@@ -1,24 +1,35 @@
 import logging
 from pathlib import Path
+import requests
 
-from src.services.fetcher import real_estate_downloader
+from src.config import REAL_ESTATE_ZIP_URL_TEMPLATE
 from src.utils.converter import convert_to_roc_year
-from src.utils.extractor import extract_zip_to_folder
-from src.utils.filter import keep_only
 
 logging.basicConfig(level=logging.WARNING)
 
 
+def real_estate_downloader(year: int, season: int) -> Path:
+    roc_year = convert_to_roc_year(year)
+
+    url = REAL_ESTATE_ZIP_URL_TEMPLATE.format(year=roc_year, season=season)
+    response = requests.get(url=url)
+    response.raise_for_status()
+
+    content_type = response.headers.get("Content-Type", "")
+    if not content_type.lower().startswith(
+        ("application/zip", "application/octet-stream")
+    ):
+        raise ValueError(
+            f"Download failed: unexpected content type. Got: {content_type}"
+        )
+
+    zip_path = Path(f"{roc_year}S{season}.zip")
+    with open(zip_path, "wb") as f:
+        f.write(response.content)
+
+    return zip_path
+
+
 def download_and_extract(year: int, season: int) -> Path:
-    roc_year = convert_to_roc_year(year=year)
-    zip_filename = real_estate_downloader(year=year, season=season)
-    zip_path = Path(zip_filename)
-
-    output_dir = Path(f"data/{roc_year}S{season}")
-    if output_dir.exists():
-        logging.warning(f"Folder already exists: {output_dir}. Skip unzip")
-    else:
-        extract_zip_to_folder(zip_path=zip_path, output_dir=output_dir)
-        _ = keep_only(folder=output_dir, suffix="_lvr_land_a.csv")
-
-    return output_dir
+    zip_path = real_estate_downloader(year=year, season=season)
+    return zip_path
